0ca51f8aa26ec59faac1a60e547dc972d5c4db2b
Jakub Piksa <kuba.piksa@wp.pl>
2023-06-10 22:18:42 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Test } from 'components/Test';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Test />
   </React.StrictMode>
 );
